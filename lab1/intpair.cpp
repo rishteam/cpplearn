a259d8ba0b0a723bed97ef7c79797318c1e8307c
roy4801 <a82611141@gmail.com>
2020-01-15 00:02:11 +0800

    Add lab1

--- lab1/intpair.cpp
+++ lab1/intpair.cpp
@@ -1,14 +1,3 @@
 #include "intpair.h"
 
 // Add code here
-
-void IntPair::set(int a, int b)
-{
-    m_a = a;
-    m_b = b;
-}
-
-void IntPair::print()
-{
-    std::printf("Pair(%d, %d)\n", m_a, m_b);
-}
--- lab1/intpair.h
+++ lab1/intpair.h
@@ -3,19 +3,3 @@
 #include <cstdio>
 #include <iostream>
 // Add code here
-
-class IntPair
-{
-private:
-    int m_a, m_b;
-public:
-    IntPair() = default;
-    IntPair(int a, int b)
-    {
-        m_a = a;
-        m_b = b;
-    }
-
-    void set(int a, int b);
-    void print();
-};
